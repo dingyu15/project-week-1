ec16c951c56deaf5276c7cbf6c8a94a2c56e9fe8
dingyu15 <wdy22@hotmail.com>
2021-09-01 23:07:42 +0800

    Created remove item function

--- to-do-list-app/src/index.js
+++ to-do-list-app/src/index.js
@@ -1,29 +1,54 @@
-
-function createItem(task)
-{
-    const item = document.createElement('div');
-    item.className = "item";
-    
-    const itemText = document.createElement ('div');
-    itemText.innerHTML = task;
-
-    item.append(itemText);
-
-    return item;
-}
-
 function addItem()
 {
-    const itemTable = document.getElementsByClassName ('list-item-section') [0];
-    if(document.getElementById('task').value == "")
+    if(document.getElementById('taskTextbox').value == "")
     {
-        alert("Please enter a task.");
+        alert('Please enter a task.');
     }
     else
     {
-        itemTable.appendChild (createItem(document.getElementById('task').value));
-        document.getElementById('task').value = '';
-        document.getElementById('task').focus();
+        const item = document.createElement('li');
+        item.innerHTML = document.getElementById('taskTextbox').value;
+        item.onclick = removeItem;
+        document.getElementById('itemList').appendChild(item);
+        document.getElementById('taskTextbox').value = '';
+        document.getElementById('taskTextbox').focus();
     }
 }
+
+function removeItem()
+{
+    document.getElementById('itemList').removeChild(this);
+}
+
+
+// function createItem(task)
+// {
+//     const item = document.createElement('div');
+//     item.id = 'itemList';
+    
+//     const itemText = document.createElement ('div');
+//     itemText.innerHTML = task;
+
+//     item.append(itemText);
+
+//     return item;   
+// }
+
+// function addItem()
+// {
+//     const itemTable = document.getElementsByClassName ('list-item-section') [0];
+//     if(document.getElementById('taskTextbox').value == "")
+//     {
+//         alert('Please enter a task.');
+//     }
+//     else
+//     {
+//         itemTable.appendChild (createItem(document.getElementById('taskTextbox').value));
+//         document.getElementById('taskTextbox').value = '';
+//         document.getElementById('taskTextbox').focus();
+//     }
+
+//     const test = document.getElementById('itemList')
+// }
+
 
